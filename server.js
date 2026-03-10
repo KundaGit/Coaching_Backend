f30daf4867a256b77345caa522a2200a032b9e86
Kundan Rajak <137685825+KundaGit@users.noreply.github.com>
2026-03-10 14:07:39 +0530

    Update email transporter to use Brevo service

--- server.js
+++ server.js
@@ -39,18 +39,13 @@ db.connect((err) => {
 // EMAIL TRANSPORTER
 // ===============================
 const transporter = nodemailer.createTransport({
-  host: "smtp.gmail.com",
+  host: "smtp-relay.brevo.com",
   port: 587,
   secure: false,
   auth: {
-    user: process.env.EMAIL_USER,
-    pass: process.env.EMAIL_PASS
-  },
-  tls: {
-    rejectUnauthorized: false
-  },
-  connectionTimeout: 20000,
-  greetingTimeout: 20000
+    user: process.env.BREVO_USER,
+    pass: process.env.BREVO_PASS
+  }
 });
 // ===============================
 // SEND OTP (EMAIL)
